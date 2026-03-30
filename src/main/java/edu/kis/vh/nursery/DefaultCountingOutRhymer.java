a69824e054fd5213e9f4f30a6a6ee3255f885777
Mateusz Gniatkowski <255590@edu.p.lodz.pl>
2026-03-30 13:09:30 +0200

    17. Dodanie dokumentacji do klasy DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,5 +1,8 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa DefaultCountingOutRhymer dostarcza bazowa funkcjonalnosc wyliczanki
+ */
 public class DefaultCountingOutRhymer {
 
     private static final int EMPTY = -1;
@@ -12,31 +15,55 @@ public class DefaultCountingOutRhymer {
 
 	private final int[] numbers = new int[MAX_SIZE];
 
+    /**
+     * Zwraca pole total
+     * @return total
+     */
     public int getTotal() {
         return total;
     }
 
     private int total = EMPTY;
 
+    /**
+     * Dodaje nową wartość do wyliczanki, jesli nie jest ona pelna.
+     * @param in Wartosc do dodania
+     */
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
+    /**
+     * Sprawdza, czy wyliczanka jest pusta.
+     * @return true, jesli wyliczanka jest pusta; w przeciwnym razie false.
+     */
     public boolean callCheck() {
         return total == EMPTY;
     }
 
+    /**
+     * Sprawdza czy wyliczanka jest pelna.
+     * @return true, jesli wyliczanka jest pelna; w przeciwnym razie false.
+     */
     public boolean isFull() {
         return total == FULL_TAB;
     }
 
+    /**
+     * Zwraca ostatnio dodana wartosc bez usuwania jej z wyliczanki.
+     * @return Ostatnia wartosc na stosie lub ERROR_VALUE, jeśli stos jest pusty.
+     */
     protected int peekABoo() {
         if (callCheck())
             return ERROR_VALUE;
         return numbers[total];
     }
 
+    /**
+     * Zwraca ostatnio dodana wartosc i usuwa ja z wyliczanki.
+     * @return Usunieta wartość lub ERROR_VALUE, jesli stos jest pusty.
+     */
     public int countOut() {
         if (callCheck())
             return ERROR_VALUE;
